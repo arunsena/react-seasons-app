7552a00397db3f2013d80e45569e5d3ab87a86e0
Arun Bojja <arunsena123@gmail.com>
2019-03-01 21:02:56 -0500

    simple to class components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    return (
-        <div class="ui container">
-            <p class="ui header">Hello World!</p>
-        </div>
-    );
+class App extends React.Component{
+    render(){
+        return (
+            <div class="ui container">
+                <p class="ui header">Hello World!</p>
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
