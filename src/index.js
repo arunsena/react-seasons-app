3e0344a829fc081b4fef597c4ce8722922ce78b8
Arun Bojja <arunsena123@gmail.com>
2019-03-02 21:46:14 -0500

    get geolocation latitude

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const getSeason = (lat, month) => {
+    if(month > 2 && month <9){
+        return lat >0 ? 'summer': 'winter';
+    }
+    else{
+        return lat >0 ? 'winter': 'summer';
+    }
+};
+
 class App extends React.Component{
+    state = {
+        season:''
+    }
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({
+                    season: getSeason(position.coords.latitude, (new Date().getMonth()+1))
+                });
+            }
+        )
+    }
+
     render(){
         return (
-            <div class="ui container">
-                <p class="ui header">Hello World!</p>
+            <div className="ui container">
+                <h1 class="ui header">{this.state.season}</h1>
             </div>
         );
     }
